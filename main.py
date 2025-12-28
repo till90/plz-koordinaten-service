94e22e9d78eecdb4feab617724b5c7dc037521a9
till90 <till.mueller1990@gmail.com>
2025-12-28 19:40:44 +0100

    add api refernce

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+API:
+- GET /api?plz=<postleitzahl>&coordsys=<system> -> Gibt die Koordinaten für eine bestimmte Postleitzahl in einem bestimmten Koordinatensystem zurück
+"""
 
 import os
 import re
